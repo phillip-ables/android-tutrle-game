a8aac633def2123c913f33c63b85507152f5c176
lip ables <phillipables1@gmail.com>
2018-09-07 17:26:54 -0400

    private reference to a bitmap, on constructor it equals bitmapFactory of decodeResource get resource, finally override onDraw

--- app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/swimmingTurtleView.java
+++ app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/swimmingTurtleView.java
@@ -1,11 +1,23 @@
 package com.example.phill.tutrle_tom_vs_the_sea_of_straws;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.View;
 
 public class swimmingTurtleView extends View {
 
+    private Bitmap turtle;
+
     public swimmingTurtleView(Context context){
         super(context);
+
+        turtle = new BitmapFactory().decodeResource(getResources(), R.drawable.fish1);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
