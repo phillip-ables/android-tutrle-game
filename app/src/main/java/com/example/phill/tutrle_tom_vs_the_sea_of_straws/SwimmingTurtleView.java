4fd3c60165b329967b255f9938270ca74508e0df
lip ables <phillipables1@gmail.com>
2018-09-07 18:13:32 -0400

    swimmingTurtle class-> Bitmap for background on create as new bitmap factory decode resources from drawable, then set color text size typeface and anti alias as true for paint for text

--- app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/SwimmingTurtleView.java
+++ app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/SwimmingTurtleView.java
@@ -4,16 +4,30 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.view.View;
 
 public class SwimmingTurtleView extends View {
 
     private Bitmap turtle;
 
+    private Bitmap backgroundImage;
+
+    private Paint scorePaint = new Paint();
+
     public SwimmingTurtleView(Context context){
         super(context);
 
         turtle = new BitmapFactory().decodeResource(getResources(), R.drawable.fish1);
+
+        backgroundImage = new BitmapFactory().decodeResource(getResources(), R.drawable.background);
+
+        scorePaint.setColor(Color.WHITE);
+        scorePaint.setTextSize(70);
+        scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
+        scorePaint.setAntiAlias(true);
     }
 
     @Override
