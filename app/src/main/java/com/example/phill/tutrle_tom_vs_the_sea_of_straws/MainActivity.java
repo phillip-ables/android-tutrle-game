7c8fd721fea3fe466523636b6cad47643882783d
lip ables <phillipables1@gmail.com>
2018-09-07 13:00:22 -0400

    a lot of updates and empty basic project

---
+++ app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.phill.tutrle_tom_vs_the_sea_of_straws;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.phill.tutrle_tom_vs_the_sea_of_straws;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
