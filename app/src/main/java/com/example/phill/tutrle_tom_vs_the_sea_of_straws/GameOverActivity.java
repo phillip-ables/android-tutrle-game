3b8cc14f973d84fe6778d6f130ee9de4f500fb53
lip ables <phillipables1@gmail.com>
2018-09-10 08:07:57 -0400

    after game over toast set intent for game over activity then add flags to the intent for new task and clear

--- app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/GameOverActivity.java
+++ app/src/main/java/com/example/phill/tutrle_tom_vs_the_sea_of_straws/GameOverActivity.java
@@ -1,13 +1,29 @@
 package com.example.phill.tutrle_tom_vs_the_sea_of_straws;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOverActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(GameOverActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
     }
 }
